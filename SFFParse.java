921668f1b6b2b537aa560cfa49da379ae988c33a
Justin Brown <jbrown23@gmail.com>
2011-07-09 16:28:38 -0500

    working header.

--- SFFParse.java
+++ SFFParse.java
@@ -66,6 +66,41 @@ public class SFFParse extends BaseParse {
 	    byte bytes[] = new byte[4];
 	    filein.read(bytes);
 	    header += ("Magic: " + new String(bytes) + "\n");
+	    filein.read(bytes);
+	    header += "Version: ";
+	    for(int i = 0; i < bytes.length; i++){
+		header += (int) bytes[i];
+	    }
+	    header += "\n";
+
+	    //header += ("Version: " + new String(bytes) + "\n");
+	    
+	    index_offset = filein.readLong();
+	    header += ("Index Offset: " + index_offset + "\n");
+	    
+	    filein.read(bytes);
+	    index_length = BytesHelper.toUint(bytes);
+	    header += ("Index Length: " + index_length + "\n");
+	    
+	    filein.read(bytes);
+	    header += ("# of Reads: " + BytesHelper.toUint(bytes) + "\n");
+
+	    int header_length = filein.readUnsignedShort();
+	    int key_length = filein.readUnsignedShort();
+	    number_of_flows_per_read = filein.readUnsignedShort();
+	    
+	    header += ("Format Code: " + filein.readUnsignedByte() + "\n");
+	    
+	    bytes = new byte[number_of_flows_per_read];
+	    filein.read(bytes);
+	    flow_chars = new String(bytes).toCharArray();
+	    
+	    bytes = new byte[key_length];
+	    filein.read(bytes);
+	    header += ("Key Sequence: " + new String(bytes) + "\n");
+	    
+	    filein.seek(header_length);
+
 	}catch(IOException ie){
             System.out.println("Error in Parsing Header");
         }
@@ -77,5 +112,6 @@ public class SFFParse extends BaseParse {
     long index_length = 0;
     int number_of_flows_per_read = 0;
     static int FLOWGRAM_BYTES_PER_FLOW = 2;
-    
+    char flow_chars[] = null;
+
 }
