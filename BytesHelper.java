4dbf4f8e102fdede1ed0da0a9c34a0ad60f9e746
Justin Brown <jbrown23@gmail.com>
2011-07-10 14:01:46 -0500

    working read.

--- BytesHelper.java
+++ BytesHelper.java
@@ -16,6 +16,13 @@ public final class BytesHelper {
 	    + (b[0] & 0xFF);
     }
 
+    public static long toUintRev( byte[] b ) {
+	return ((b[0] << 24) & 0xFF)
+	    + ((b[1] & 0xFF) << 16)
+	    + ((b[2] & 0xFF) << 8)
+	    + (b[3] & 0xFF);
+    }
+
     public static int shortToInt( byte[] b ) {
 	return ((b[1]) << 8)
 	    + (b[0] & 0xFF);
--- SFFParse.java
+++ SFFParse.java
@@ -24,32 +24,103 @@ public class SFFParse extends BaseParse {
 	    offset = filein.getFilePointer();
 	    if(offset == index_offset){
 		filein.seek(offset + index_length);
+		offset = filein.getFilePointer();
 	    }
-
-	    byte buffer[] = new byte[2];
-	    filein.read(buffer);
-	    int read_header_length = BytesHelper.shortToInt(buffer);
-	    offset = filein.getFilePointer() + read_header_length - 2;
-	    buffer = new byte[4];
-	    filein.read(buffer);
-	    long number_of_bases = BytesHelper.toUint(buffer);
+	    	    
+	    int read_header_length = filein.readUnsignedShort();
+            System.out.println("read header length " + read_header_length);
+	    int name_length = filein.readUnsignedShort();
 	    
-	    filein.seek(offset);
+	    byte buffer[] = new byte[4];
+	    filein.read(buffer);
+	    long number_of_bases = BytesHelper.toUintRev(buffer);
+	    System.out.println("num bases " + number_of_bases);
+
+	    filein.seek(offset + read_header_length);
 	    long read_data_size = FLOWGRAM_BYTES_PER_FLOW*number_of_flows_per_read + 3*number_of_bases;
-	    if((read_data_size & 8) > 0){
+	    if((read_data_size & 7) > 0){
+		System.out.println("got here!!!");
 		read_data_size = (((read_data_size >> 3)+1) <<3);
 	    }
+	    System.out.println("read data size is " + read_data_size);
 	    
-	    filein.seek(offset + read_data_size);
-	    return (offset + read_data_size);
+	    filein.seek(offset + read_header_length + read_data_size);
+	    return offset;
 	}catch(Exception e){
 	    return -1;
 	}
     }
     
     public String[] getNextRecord(){
-	return null;
+	ArrayList<String> fields = new ArrayList<String>();
 	
+	try{
+	    long offset = filein.getFilePointer();
+	    int read_header_length = filein.readUnsignedShort();
+	    int name_length = filein.readUnsignedShort();
+
+	    System.out.println("A " + read_header_length);
+	    System.out.println(name_length);
+
+	    int number_of_bases = filein.readInt();
+	    System.out.println(number_of_bases);
+
+	    int clip_qual_left = filein.readUnsignedShort();
+	    int clip_qual_right = filein.readUnsignedShort();
+	    int clip_adapter_left = filein.readUnsignedShort();
+	    int clip_adapter_right = filein.readUnsignedShort();
+
+	    System.out.println(clip_qual_left);
+	    System.out.println(clip_qual_right);
+	    System.out.println(clip_adapter_left);
+	    System.out.println(clip_adapter_right);
+
+	    byte buffer[] = new byte[name_length];
+	    filein.read(buffer);
+            char Name[] = new String(buffer).toCharArray();
+
+	    System.out.println(Name);
+
+	    filein.seek(offset + read_header_length);
+	    long read_data_size = FLOWGRAM_BYTES_PER_FLOW*number_of_flows_per_read + 3*number_of_bases;
+            if((read_data_size & 7) > 0){
+                read_data_size = (((read_data_size >> 3)+1) <<3);
+            }
+
+	    //???
+	    //int values[] = new int[number_of_flows_per_read];
+	    //for(int i = 0; i < values.length; i++){
+	    //values[i] = filein.readUnsignedShort();
+	    //}
+
+	    //???
+	    //filein.skipBytes(2*number_of_flows_per_read);
+	    filein.seek(filein.getFilePointer() + 2*number_of_flows_per_read + number_of_bases);
+	    
+	    buffer = new byte[number_of_bases];
+	    filein.read(buffer);
+	    char Bases[] = new String(buffer).toCharArray();
+	    
+	    byte qual[] = new byte[number_of_bases];
+	    filein.read(qual);
+	    for(int i = 0; i < qual.length; i++){
+		qual[i] += 33;
+	    }
+	    
+	    //Go to end of read block.
+	    filein.seek(offset + read_header_length + read_data_size);
+            
+	    fields.add(new String(Name));
+	    fields.add(new String(Bases));
+	    fields.add(new String(qual));
+
+	}catch(Exception e){
+            return null;
+        }
+	
+	String fa[] = new String[fields.size()];
+	return fields.toArray(fa);
+		
     }
 
     public double getProgress(){
@@ -72,20 +143,20 @@ public class SFFParse extends BaseParse {
 		header += (int) bytes[i];
 	    }
 	    header += "\n";
-
-	    //header += ("Version: " + new String(bytes) + "\n");
 	    
 	    index_offset = filein.readLong();
 	    header += ("Index Offset: " + index_offset + "\n");
 	    
 	    filein.read(bytes);
-	    index_length = BytesHelper.toUint(bytes);
+	    index_length = BytesHelper.toUintRev(bytes);
 	    header += ("Index Length: " + index_length + "\n");
 	    
 	    filein.read(bytes);
-	    header += ("# of Reads: " + BytesHelper.toUint(bytes) + "\n");
+	    header += ("# of Reads: " + BytesHelper.toUintRev(bytes) + "\n");
 
 	    int header_length = filein.readUnsignedShort();
+	    System.out.println("header length: " + header_length);
+
 	    int key_length = filein.readUnsignedShort();
 	    number_of_flows_per_read = filein.readUnsignedShort();
 	    
